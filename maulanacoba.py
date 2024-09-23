e5cef5e8837b24dd96ff1168a095a7f479278d26
Maulanacx <maulana.prayoga.2203226@students.um.ac.id>
2024-09-23 10:11:54 +0700

    Update maulanacoba.py

--- maulanacoba.py
+++ maulanacoba.py
@@ -3,10 +3,11 @@ import streamlit as st
 x = st.number_input("masukkan angka")
 sx = st.text_input("satuan", "C")
 st.write("Anda memasukkan", x,' ',sx)
-sy = st.text_input("Dikonversi ke", "K")
+sy = st.text_input("Dikonversi ke", "C")
 y = 0
 if (sx == 'C'):
  if(sy == 'C'):
   y = x
  elif(sy == 'F'):
+  9/5*C+32
 st.write(x,' ',sx, '= ',y, sy)
