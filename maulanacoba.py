16c23eb511d2871c005d4966ad5a1f0553e93628
Maulanacx <maulana.prayoga.2203226@students.um.ac.id>
2024-09-23 10:55:24 +0700

    Update maulanacoba.py

--- maulanacoba.py
+++ maulanacoba.py
@@ -1,25 +1,47 @@
 import streamlit as st
 
 x = st.number_input("masukkan angka")
-sx = st.text_input("satuan", "C")
+sx = st.selecbox("satuan", ("C","F","K","R"), key='sx')
 st.write("Anda memasukkan", x,' ',sx)
-sy = st.text_input("Dikonversi ke", "C")
+sy = st.selecbox("Dikonversi ke", ("C","F","K","R"), key='sy')
 y = 0
 if (sx == 'C'):
  if(sy == 'C'):
   y = x
  elif(sy == 'F'):
   y = x*9/5+32
-y = 0
-if (sx == 'C'):
- if(sy == 'C'):
-  y = x
  elif(sy == 'K'):
-  y = x+273,15
-y = 0
-if (sx == 'C'):
- if(sy == 'C'):
-  y = x
+  y = x+273,15 
  elif(sy == 'R'):
-  y = x*4/5  
+  y = x*4/5 
+y = 0
+elif (sx == 'F'):
+ if(sy == 'F'):
+  y = x
+ elif(sy == 'C'):
+  y = (x-32)*5/9
+ elif(sy == 'R'):
+  y = 4/9*(x-32) 
+ elif(sy == 'K'):
+  y = (x+459,67)*5/9
+y = 0
+elif (sx == 'R'):
+ if(sy == 'R'):
+  y = x
+ elif(sy == 'C'):
+  y = x*5/4
+ elif(sy == 'F'):
+  y = 9/4*x+32 
+ elif(sy == 'K'):
+  y = 273,15+(x*5/4)
+y = 0
+elif (sx == 'K'):
+ if(sy == 'K'):
+  y = x
+ elif(sy == 'C'):
+  y = x-273,15
+ elif(sy == 'F'):
+  y = 9/5*x-459,67
+ elif(sy == 'R'):
+  y = (273,15-x)*4/5
 st.write(x,' ',sx, '= ',y, sy)
