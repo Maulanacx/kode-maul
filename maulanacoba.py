f6ea2d0337195075044cf818d1dfe187024b07d4
Maulanacx <maulana.prayoga.2203226@students.um.ac.id>
2024-09-23 15:52:16 +0700

    Update maulanacoba.py

--- maulanacoba.py
+++ maulanacoba.py
@@ -7,41 +7,41 @@ sy = st.selectbox("Dikonversi ke", ("C", "F", "R", "K"), key='sy')
 
 y = 0
 
-if sx == 'C':  # Celsius
-    if sy == 'C':
+if (sx == 'C'):  # Celsius
+    if (sy == 'C'):
         y = x
-    elif sy == 'F':  
+    elif (sy == 'F'):  
         y = (9/5) * x + 32
-    elif sy == 'K':  
+    elif (sy == 'K'):  
         y = x + 273
-    elif sy == 'R': 
+    elif (sy == 'R'): 
         y = x * 4/5
-elif sx == 'F':  
-    if sy == 'C':  
+elif (sx == 'F'):  
+    if (sy == 'C'):  
         y = (x - 32) * 5/9
-    elif sy == 'F':
+    elif (sy == 'F'):
         y = x
-    elif sy == 'K': 
+    elif (sy == 'K'): 
         y = (x - 32) * 5/9 + 273
     elif sy == 'R':  
         y = (x - 32) * 4/9
-elif sx == 'K':  
-    if sy == 'C':  
+elif (sx == 'K'):  
+    if (sy == 'C'):  
         y = x - 273
-    elif sy == 'F':  
+    elif (sy == 'F'):  
         y = (x - 273) * 9/5 + 32
-    elif sy == 'K':
+    elif (sy == 'K'):
         y = x
-    elif sy == 'R':  
+    elif (sy == 'R'):  
         y = (x - 273) * 0.8
-elif sx == 'R':  # Reaumur
-    if sy == 'C':  
+elif (sx == 'R'):  # Reaumur
+    if (sy == 'C'):  
         y = x / 0.8
-    elif sy == 'F':  
+    elif (sy == 'F'):  
         y = (x * 9/4) + 32
-    elif sy == 'K': 
+    elif (sy == 'K'): 
         y = (x / 0.8) + 273
-    elif sy == 'R':
+    elif (sy == 'R'):
         y = x
 
-st.write(x, sx," = ",y, sy)
+st.write(x, sx, "=" ,y, sy)
