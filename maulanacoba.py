cdd0590eeac9175bdd34fd160ad724341ccd4fcd
Maulanacx <maulana.prayoga.2203226@students.um.ac.id>
2024-09-23 10:58:43 +0700

    Update maulanacoba.py

--- maulanacoba.py
+++ maulanacoba.py
@@ -1,44 +1,47 @@
 import streamlit as st
 
-x = st.number_input("masukkan angka")
-sx = st.selecbox("satuan", ("C","F","K","R"), key='sx')
-st.write("Anda memasukkan", x,' ',sx)
-sy = st.selecbox("Dikonversi ke", ("C","F","K","R"), key='sy')
+x = st.number_input("Masukkan angka")
+sx = st.selectbox("Satuan", ("C", "F", "R", "K"), key='sx')
+st.write ("Anda memasukkan", x,'',sx)
+sy = st.selectbox("Dikonversi ke", ("C", "F", "R", "K"), key='sy')
+
 y = 0
-if (sx == 'C'):
- if(sy == 'C'):
-  y = x
- elif(sy == 'F'):
-  y = x*9/5+32
- elif(sy == 'K'):
-  y = x+273,15 
- elif(sy == 'R'):
-  y = x*4/5 
-elif (sx == 'F'):
- if(sy == 'F'):
-  y = x
- elif(sy == 'C'):
-  y = (x-32)*5/9
- elif(sy == 'R'):
-  y = 4/9*(x-32) 
- elif(sy == 'K'):
-  y = (x+459,67)*5/9
-elif (sx == 'R'):
- if(sy == 'R'):
-  y = x
- elif(sy == 'C'):
-  y = x*5/4
- elif(sy == 'F'):
-  y = 9/4*x+32 
- elif(sy == 'K'):
-  y = 273,15+(x*5/4)
-elif (sx == 'K'):
- if(sy == 'K'):
-  y = x
- elif(sy == 'C'):
-  y = x-273,15
- elif(sy == 'F'):
-  y = 9/5*x-459,67
- elif(sy == 'R'):
-  y = (273,15-x)*4/5
-st.write(x,' ',sx, '= ',y, sy)
+
+if sx == 'C':  # Celsius
+    if sy == 'C':
+        y = x
+    elif sy == 'F':  
+        y = (9/5) * x + 32
+    elif sy == 'K':  
+        y = x + 273
+    elif sy == 'R': 
+        y = x * 4/5
+elif sx == 'F':  
+    if sy == 'C':  
+        y = (x - 32) * 5/9
+    elif sy == 'F':
+        y = x
+    elif sy == 'K': 
+        y = (x - 32) * 5/9 + 273
+    elif sy == 'R':  
+        y = (x - 32) * 4/9
+elif sx == 'K':  
+    if sy == 'C':  
+        y = x - 273
+    elif sy == 'F':  
+        y = (x - 273) * 9/5 + 32
+    elif sy == 'K':
+        y = x
+    elif sy == 'R':  
+        y = (x - 273) * 0.8
+elif sx == 'R':  # Reaumur
+    if sy == 'C':  
+        y = x / 0.8
+    elif sy == 'F':  
+        y = (x * 9/4) + 32
+    elif sy == 'K': 
+        y = (x / 0.8) + 273
+    elif sy == 'R':
+        y = x
+
+st.write(x, sx, "=", y, sy)
