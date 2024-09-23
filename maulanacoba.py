5707737f56e0a86500f7ad0933a2598236ce0219
Maulanacx <maulana.prayoga.2203226@students.um.ac.id>
2024-09-23 10:07:29 +0700

    Update maulanacoba.py

--- maulanacoba.py
+++ maulanacoba.py
@@ -4,9 +4,10 @@ x = st.number_input("masukkan angka")
 sx = st.text_input("satuan", "C")
 st.write("Anda memasukkan", x,' ',sx)
 sy = st.text_input("Dikonversi ke", "K")
- if celcius >= 0:
-   kelvin = celcius + 273
-   return kelvin
- else :
-   return "suhu harus positif"
+y = 0
+if (sx == 'C'):
+ if(sy == 'C'):
+  y = x
+ elif(sy == 'F'):
+  pass
 st.write(x,' ',sx, '= ...', sy)
