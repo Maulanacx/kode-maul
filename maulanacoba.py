8e25674777211a0b6eb95e518ac74de6629d0f63
Maulanacx <maulana.prayoga.2203226@students.um.ac.id>
2024-09-23 09:57:44 +0700

    Update maulanacoba.py

--- maulanacoba.py
+++ maulanacoba.py
@@ -3,6 +3,6 @@ import streamlit as st
 x = st.number_input("masukkan angka")
 sx = st.text_input("satuan", "C")
 st.write("Anda memasukkan", x,' ',sx)
-sy = st.text_input("Dikonversi ke", "C")
+sy = st.text_input("Dikonversi ke", "K")
 
 st.write(x,' ',sx, '= ...', sy)
